8e8aff80eea7f181f071dd19db9e85087f75f9ee
Carla Barragan <ccbv29@gmail.com>
2025-09-18 23:34:28 +0000

    capmaings added

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,18 @@
+SELECT
+  f.date,
+  f.operational_margin,
+  c.ads_cost,
+  f.operational_margin - c.ads_cost AS ads_margin,
+  f.average_basket,
+  c.ads_impression,
+  c.ads_clicks,
+  f.quantity,
+  f.revenue,
+  f.purchase_cost,
+  f.margin,
+  f.shipping_fee,
+  f.log_cost,
+  f.ship_cost
+FROM {{ ref('finance_days') }} f
+LEFT JOIN {{ ref('int_campaigns_day') }} c
+ON f.date = c.date
---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+        date_date as date_date,
+        paid_source as paid_source,
+        campaign_key as campaign_key,
+        campgn_name as campaign_name,
+        cast(ads_cost as float64) as ads_cost,
+        impression as impression,
+        click as click
+
+    from source
+
+)
+
+select * from renamed
